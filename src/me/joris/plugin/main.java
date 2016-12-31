f31ea68eda144023e6de5d06f543fcc31c63a915
j.v.aalstlive.nl <j.v.aalstlive.nl@192.168.178.10>
2016-12-31 16:31:26 +0100

    Update 0.2

--- src/me/joris/plugin/main.java
+++ src/me/joris/plugin/main.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin {
 	
 	public void onEnable(){
-		
+	   System.out.println("Joris is cool");
 	}
 	
 	public void onDisable(){
