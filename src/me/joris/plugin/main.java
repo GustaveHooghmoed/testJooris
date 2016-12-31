cbbc87882b23ad68867eb17834fa4e400702ba93
Administrator <Administrator@TRANSIP-VPS>
2016-12-31 15:24:31 +0100

    Main

--- src/me/joris/plugin/main.java
+++ src/me/joris/plugin/main.java
@@ -1,5 +1,15 @@
 package me.joris.plugin;
 
-public class main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
 
 }
